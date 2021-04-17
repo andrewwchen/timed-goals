80adc112bebf543616a90f8707908e7a6b7ad186
chenmasterandrew <andrewac2015@gmail.com>
2021-04-17 17:32:49 -0400

    progress bar

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const libCommands = require('./libCommands')
+const data = require('./testData.json')
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -25,6 +27,7 @@ function activate(context) {
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('timedgoals.createProgressBar', libCommands.createProgressBar(data.goals[0])));
 }
 
 // this method is called when your extension is deactivated
