7459d2bce860a3a341bfcb4ea0f88874df5b6db8
Alphonso Bradham <bradhamalphonso@gmail.com>
2021-04-17 17:21:10 -0400

    Added addTimedGoal functionality

--- libCommands.js
+++ libCommands.js
@@ -1,3 +1,44 @@
 // VSCode import
 const vscode = require('vscode');
 
+/*
+* addTimedGoal: A function that takes the workplace context and a new timedGoal
+* json object and adds it to the ExtensionContext.workspacestate data store.
+* Returns void.
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function addTimedGoal(context, newTimedGoal){
+    // Get current goals state
+    updatedGoals = context.globalState.get('data').goals;
+    
+    // Add new goal to copy of present state
+    updatedGoals.push(newTimedGoal);
+
+    // Update global state with new data appended
+    context.globalState.update('data', {goals: updatedGoals});
+}
+
+
+/*
+* createTimedGoal: Creates a json object to be saved to the global data store
+* from a list of passed parameters
+* @param time Date
+* @param name String
+* @param duration int
+* @param complete boolean
+*/
+
+function createTimedGoal(time, name, duration, complete){
+    return{
+            name: name,
+            time: time,
+            duration: duration,
+            complete: complete
+        }
+}
+
+module.exports = {
+    createTimedGoal,
+    addTimedGoal
+}
